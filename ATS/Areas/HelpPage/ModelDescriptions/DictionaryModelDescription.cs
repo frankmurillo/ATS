a97e9da37d93037b710b9038242f39835a24185c
burton.wu@wforce.org <123456visual>
2015-03-27 17:13:18 -0400

    inital commit

---
+++ ATS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ATS.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ATS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ATS.WebApiApplication" Language="C#" %>
